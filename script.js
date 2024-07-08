c1733b2c2b7ffaedb43514c84ee9e2c146c9914c
adishU <03adish@gmail.com>
2024-07-08 16:34:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Welcow");
+
+let value = 10;
+let secValue = 20;
+let output = value + secValue;
+console.log(output);
+{
+    let value = 7
+    console.log(value)
+}
+console.log('The value of the outside value is: ', value)
